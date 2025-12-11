58feb6b37cdd5602931e1ba7bb61f8ccbf3a0666
Y-Ar3MyPantsWet <526abrigante@frhsd.com>
2025-12-11 02:05:06 +0000

    step five done binded the palceholdesd

--- site.js
+++ site.js
@@ -6,6 +6,23 @@ const vue_app = Vue.createApp({
             github: "https://github.com/username/repo",
             movies: []
         }
+    },
+    methods: {
+        makeTextDate(dateArray) {
+            const [year,month,day] = dateArray;
+            return `${month}/${day}/${year}`;
+        },
+        timeText(minutes) {
+            const h = Math.floor(minutes/60);
+            const m = minutes%60;
+            return `${h}h ${m}m`;
+        },
+        like(index){ this.movies[index].likes++; },
+        dislike(index){ this.movies[index].dislikes++; },
+        posterClick(index){
+            let movie=this.movies[index];
+            movie.posterindex=(movie.posterindex+1)%movie.posters.length;
+        }
     }
 });
 vue_app.mount("#vue_app");
